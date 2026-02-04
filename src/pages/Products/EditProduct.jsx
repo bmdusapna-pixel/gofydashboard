1abbfbd462bed270c7bba6ffcebb335c4ff23663
Naveen Dangwal <naveendangwal22@gmail.com>
2026-02-04 17:55:06 +0530

    updates

--- src/pages/Products/EditProduct.jsx
+++ src/pages/Products/EditProduct.jsx
@@ -94,7 +94,7 @@ const EditProduct = () => {
 
   const fetchCoupons = async () => {
     try {
-      const response = await api.get(`/api/offers`);
+      const response = await api.get(`offers`);
       const result = await response.json();
       console.log(result.coupons);
       setCoupons(result.coupons);
